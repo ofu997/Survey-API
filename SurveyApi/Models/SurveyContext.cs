574136b4ca0986d4b047c9b0163a2d3bc164bde9
oliver fu <ofu997@gmail.com>
2021-03-10 20:17:29 -0800

    add Survey class and context for EF

---
+++ SurveyApi/Models/SurveyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SurveyApi.Models
+{
+    public class SurveyContext : DbContext
+    {
+        public SurveyContext(DbContextOptions<SurveyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SurveyItem> SurveyItems { get; set; }
+
+    }
+}
---
+++ SurveyApi/Models/SurveyItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SurveyApi.Models
+{
+    public class SurveyItem
+    {
+        public long Id { get; set; }
+        public string Review { get; set; }
+
+        [Range(0, 10,
+        ErrorMessage = "Value for rating must be 0 - 10")]
+        public int ProductRating { get; set; }
+
+        [Range(0, 10,
+        ErrorMessage = "Value for rating must be 0 - 10")]
+        public int CompanyRating { get; set; }
+        public bool ResearchedBefore { get; set; }
+
+        public bool ArrivedOnTime { get; set; }
+    }
+}
